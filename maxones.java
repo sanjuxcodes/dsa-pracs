51c65619806ef416c62bc1526dcefc252d3ef24e
Sanjoy Das <145825149+sanjuxcodes@users.noreply.github.com>
2025-12-21 00:51:49 +0530

    Add files via upload

---
+++ maxones.java
@@ -0,0 +1,30 @@
+public class maxones {
+    static int findd(int arr[]){
+        int cnt=0;
+        int maxi=0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i]==1){
+                cnt++;
+                maxi=Math.max(maxi, cnt);
+            }
+            else 
+                cnt=0;
+           
+              
+                
+            
+        }
+
+
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,1,0,1,1,1,0,0,1,0,1,1,1,1};
+        System.out.println(findd(arr));
+    }
+    
+}
+//arr=[1,1,0,0,1,1,1,0,1,1,0,1]
---
+++ rmvdupli.java
@@ -0,0 +1,17 @@
+public class rmvdupli {
+    static int findd(int arr[]){
+        int xor=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            xor^=arr[i];
+        }
+        return xor;
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,2,3,3,4,4};
+        System.out.println(findd(arr));
+    }
+    
+}
+//arr=[1,1,2,3,3,4,4]
