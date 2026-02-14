28a345727b087e2dea27b63eaa48eeee849a6990
Sanjoy Das <145825149+sanjuxcodes@users.noreply.github.com>
2026-02-14 16:58:48 +0530

    Add files via upload

---
+++ insert.java
@@ -0,0 +1,17 @@
+public class insert {
+    
+    static int findd(int arr[],int k){
+
+        int l =0;
+        int h=arr.length-1;
+
+        while(l<=h){
+
+            int m =(l+h)/2;
+
+
+        }
+        return 1;
+
+    }
+}
